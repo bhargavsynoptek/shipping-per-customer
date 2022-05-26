f9397ee1e78b21d99536608a9833afa0f9977bcb
root <root@infoanalytica.local>
2022-05-26 16:51:29 +0530

    removed wrong package

--- Syn/ShippingPerCustomer/registration.php
+++
@@ -1,4 +0,0 @@
-<?php
-use \Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Syn_ShippingPerCustomer', __DIR__);
--- Syn/ShippingPerCustomer/view/adminhtml/web/js/updateItems.js
+++
@@ -1,7 +0,0 @@
-require(['jquery', 'jquery/ui'], function ($) {
-    jQuery(document).ready(function () {
-        $(document).on('change', 'select#group_id', function () {
-            order.itemsUpdate();
-        });
-    });
-});
